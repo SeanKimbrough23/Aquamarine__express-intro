def53dd3d386d6bb0f00564079c9474bc178e7b7
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-03-01 16:31:39 -0600

    updated from lecture notes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,14 +5,39 @@ function onReady(){ // 2
 
     // function to get quotes from server
     getQuotes(); // 3
+
+    // 8  listener for newQuoteButton
+    $('#newQuoteButton').on('click' , addQuote)
 }
+function addQuote(){
+    console.log('Inside add quote' );
+
+    //  9  make a POST request to server
+    $.ajax({
+        method: 'POST', //the type of request
+        url: '/',
+        data: {
+            text: "we are farmers", //req.body.text
+            author: "farmers insurance" // req.body.author
+        }
+    }).then(function(response){ // this can also be an arrow function .then((response) => )
+        console.log('Yayaya, POST request successful!');
+        getQuotes()
+    }).catch(function(response){
+        alert('request failed! (Post to /)')
+    })
+}
+
 function getQuotes(){ // 4
     //Use AJAX to get quotes from server 
     $.ajax({
         method: 'GET', //this matches app.get('/quotes', function(req, res){} from our server.js file
         url: '/quotes' // 5  <---- this is the route we are telling it to follow from t:5000/quotes
     }).then(function(response ){ // this is getting the res.send(quoteList); from the server.js file (this is our HAPPY Path)
-        console.log('Success!!!!', response) 
+        console.log('Success!!!!', response) // 10 response will be quoteList from server
+                                            // if quote list was added  to by POST request,
+                                            // then it will include thee new quotes
+
         // pass response of quotes into render function
         // this will be quoteList in this example  
         renderToDom(response) // 7
--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express'); // gives us acces to a function from express
+const bodyParser = require('body-parser'); // 9. we are requiring body Parser from package.json like express 
+
 
 // 1 Create an instance of express by calling the function.
 const app = express();
@@ -7,10 +9,19 @@ const port = 5000; // for instances where you are working with different servers
 // 2 Now we want to point this app to our public folder
 app.use(express.static('server/public')); // giving it a path to our public folder so it is available
 
+// we are configuring our instance of the server to use bodyparser
+app.use(bodyParser.urlencoded({extended:true})); //10
+
+//  12 This moves our quoteList into a different file 
+ //const quoteList = require('./modules/quoteList'); // 8. example after we moved quoteList into separate file called quoteList.js, now we are requiring it
+ let quoteList = [ // 4
+ {text: "Nationwide is on your side", author: "Nationwide"},
+ {text: "Just Do It!", author: 'Nike'},
+ {text: "You're In Good Hands", author:"Allstate"}
+];
 
 
- const quoteList = require('./modules/quoteList'); // 8. example after we moved quoteList into separate file called quoteList.js, now we are requiring it
-// 5 To create a GET request
+ // 5 To create a GET request
 // Normal URL is http://localhost:5000/
 // To use GET request: http://localhost:5000/quotes
 app.get('/quotes', function(req, res){ // req = request, res= response
@@ -27,7 +38,35 @@ res.send(quoteList); // We can only send 1 response per request, anything after 
 //Respond with status code & a thing
 // res.send(400).send('this request is broken'); ---> this is just to show that it doesn't work
 
-}) // in this function we have a request to GET quotes List array and we then get a response of server sending back the quote list
+}) ;// in this function we have a request to GET quotes List array and we then get a response of server sending back the quote list
+
+// 11 Route for POST request 
+app.post('/', (req, res) => {
+console.log('Does this work POST 🥸');
+
+// this is grabbing a new quote from the request body
+// The request body is the data that is sent from the client 
+    let incomingText = req.body.text  // (how we access data) the last part .text comes from PostMan (whatever we title in POSTMAN under body under key will go here ) we have text in key box on PostMan
+    let incomingAuthor = req.body.author //(how we access data) the last part .author comes from PostMan (whatever we title in POSTMAN under body under key will go here ) we have author in key box on PostMan
+
+console.log('Incoming quote to add', incomingText); // to see if let quote = req.body.text is working
+console.log('Incoming author to add', incomingAuthor); // to see if let author = req.body.author is working
+
+//this is a new quote object
+// which will be pushed to the quotesList array
+
+const newQuote = {
+    text: incomingText,
+    author: incomingAuthor
+};
+quoteList.push(newQuote);
+console.log('All quotes:' , quoteList)
+
+
+    res.sendStatus(201);
+})
+
+
 
 
 // 6. After this function is written.. restart server in terminal with ctrl C and refresh browser with localhost:5000/quotes <---- this text would change depending on what we are GET
